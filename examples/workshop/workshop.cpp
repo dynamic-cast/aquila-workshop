dfb0952c4d3c6106ee9c6b39d07dd58328e407d6
aniawsz <me@ania.ws>
2022-11-14 00:59:57 +0000

    Fix the problem in the calling code

--- examples/workshop/workshop.cpp
+++ examples/workshop/workshop.cpp
@@ -1,7 +1,25 @@
+#include "aquila/functions.h"
 #include "aquila/source/WaveFile.h"
 #include "aquila/transform/Mfcc.h"
 
+#include <algorithm>
 #include <iostream>
+#include <vector>
+
+namespace
+{
+
+std::size_t roundToPowerOf2(std::size_t n)
+{
+    if (Aquila::isPowerOf2(n))
+    {
+        return n;
+    }
+
+    return Aquila::nextPowerOf2(n);
+}
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,9 +32,15 @@ int main(int argc, char *argv[])
     Aquila::WaveFile wav(argv[1]);
 
     const auto numSamples = wav.getSamplesCount();
+    const auto fftSize = roundToPowerOf2(numSamples);
 
-    Aquila::Mfcc mfcc(numSamples);
-    const auto mfccValues = mfcc.calculate(wav);
+    auto paddedData = std::vector<Aquila::SampleType>(fftSize);
+    std::copy_n(wav.begin(), numSamples, paddedData.begin());
+
+    const auto paddedSignal = Aquila::SignalSource(paddedData, wav.getSampleFrequency());
+
+    Aquila::Mfcc mfcc(fftSize);
+    const auto mfccValues = mfcc.calculate(paddedSignal);
 
     for (const auto val : mfccValues)
     {
